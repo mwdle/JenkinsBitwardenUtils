bbb3481152cd3d7abed9caaf1d043e9b04d8e370
mwdle <mwdle@mwdle.com>
2025-08-30 13:13:26 -0600

    Changed away from using Groovy Properties class to parse and load .env files from bitwarden secure notes in favor of custom parsing that is more similar to Docker and Shell behavior. Improved error handling

--- vars/withBitwardenEnv.groovy
+++ vars/withBitwardenEnv.groovy
@@ -1,16 +1,21 @@
 def call(Map config, Closure body) {
     // Call the main withBitwarden function and automatically parse environment variables
     withBitwarden(config) { credential ->
-        // Parse environment variables / properties from secure notes
+        // Parse environment variables (Docker .env) from secure note -- Mimics behavior outlined in https://docs.docker.com/compose/how-tos/environment-variables/variable-interpolation/
         def envList = []
         if (credential.notes) {
-            def props = new Properties()
-            props.load(new StringReader(credential.notes))
-            props.each { key, value ->
-                envList.add(key + "=" + value)
+            credential.notes.split('\n').each { line ->
+                def trimmedLine = line.trim()
+                if (trimmedLine =~ /^[A-Z0-9_]+=.*$/) {
+                    if (trimmedLine.contains('"') || trimmedLine.contains("'"))
+                        envList.add(trimmedLine)
+                    else
+                        envList.add(trimmedLine.split(' #', 2)[0].trim()) // Remove inline comments if the value has no quotations and the comment character (#) is preceded by a space
+                }
             }
+        } else {
+            error("Error: The 'notes' field in the specified Bitwarden credential JSON is missing or empty.")
         }
-        
         // Execute the closure with environment variables set
         withEnv(envList) {
             body()
