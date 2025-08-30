cd8de6a2871272abcf228e9a6bae36ce3b9a420a
mwdle <mwdle@mwdle.com>
2025-08-30 00:13:09 -0600

    test=

--- vars/withBitwarden.groovy
+++ vars/withBitwarden.groovy
@@ -8,7 +8,7 @@ def call(Map config, Closure body) {
     def bitwardenServerUrl = config.bitwardenServerUrl ?: env.BITWARDEN_SERVER_URL
     def apiKeyCredentialId = config.apiKeyCredentialId ?: 'bitwarden-api-key'
     def masterPasswordCredentialId = config.masterPasswordCredentialId ?: 'bitwarden-master-password'
-    def credential
+    // def credential
     withCredentials([
         usernamePassword(credentialsId: apiKeyCredentialId, usernameVariable: 'BW_CLIENTID', passwordVariable: 'BW_CLIENTSECRET'),
         string(credentialsId: masterPasswordCredentialId, variable: 'BITWARDEN_MASTER_PASSWORD')
@@ -41,12 +41,13 @@ def call(Map config, Closure body) {
                     ''',
                     returnStdout: true
                 ).trim()
-                credential = readJSON text: credentialJson
+                def credential = readJSON text: credentialJson
+                body(credential)
             }
         } finally {
             sh 'bw logout || true' // Always logout even after failure
         }
     }
-    // Closure does not execute within the `withCredentials` block for security reasons
-    body(credential)
+    // // Closure does not execute within the `withCredentials` block for security reasons
+    // body(credential)
 }
