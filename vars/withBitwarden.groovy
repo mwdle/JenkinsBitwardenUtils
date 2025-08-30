00f4f4ee63b3b160d75b5a22f0674252e2ddc9ec
mwdle <mwdle@mwdle.com>
2025-08-29 23:46:17 -0600

    testing old code again

--- vars/withBitwarden.groovy
+++ vars/withBitwarden.groovy
@@ -1,6 +1,6 @@
 // Retrieves a Bitwarden item and executes closure with the credential object
 // Requires: itemName parameter
-// Optional: bitwardenServerUrl (default: 'https://vault.bitwarden.com'), apiKeyCredentialId (default: 'bitwarden-api-key'), masterPasswordCredentialId (default: 'bitwarden-master-password')
+// Optional: bitwardenServerUrl (defaults to Bitwarden CLI default), apiKeyCredentialId (default: 'bitwarden-api-key'), masterPasswordCredentialId (default: 'bitwarden-master-password')
 def call(Map config, Closure body) {
     if (!config.itemName) {
         error 'withBitwarden: itemName parameter is required'
@@ -14,36 +14,34 @@ def call(Map config, Closure body) {
         string(credentialsId: masterPasswordCredentialId, variable: 'BITWARDEN_MASTER_PASSWORD')
     ]) {
         try {
-            if (bitwardenServerUrl)
-                sh "bw config server ${bitwardenServerUrl}"
-            sh 'bw login --apikey'
-            def sessionToken = sh(
-                    script: 'bw unlock --raw --passwordenv BITWARDEN_MASTER_PASSWORD',
-                    returnStdout: true
-                ).trim()
-            // Provides the secrets to the shell command using environment variables and no groovy interpolation to maximize security
-            withEnv(["ITEM_NAME=${config.itemName}", "SESSION_TOKEN=${sessionToken}"]) {
+            // if (bitwardenServerUrl)
+            //     sh "bw config server ${bitwardenServerUrl}"
+            // sh 'bw login --apikey'
+            // def sessionToken = sh(
+            //         script: 'bw unlock --raw --passwordenv BITWARDEN_MASTER_PASSWORD',
+            //         returnStdout: true
+            //     ).trim()
+            // // Provides the secrets to the shell command using environment variables and no groovy interpolation to maximize security
+            // withEnv(["ITEM_NAME=${config.itemName}", "SESSION_TOKEN=${sessionToken}"]) {
+            //     credential = readJSON text: sh(
+            //         script: 'set +x; bw get item "$ITEM_NAME" --session "$SESSION_TOKEN"', // `set +x` prevents printing the shell command to the log to avoid exposing `$SESSION_TOKEN`
+            //         returnStdout: true
+            //     ).trim()
+            // }
+
+            withEnv(["ITEM_NAME=${config.itemName}"]) {
                 credential = readJSON text: sh(
-                    script: 'set +x; bw get item "$ITEM_NAME" --session "$SESSION_TOKEN"', // `set +x` prevents printing the shell command to the log to avoid exposing `$SESSION_TOKEN`
+                    // Redirects output from most commands to avoid polluting the captured secret JSON
+                    script: '''
+                        set +x # Don't echo commands in logs
+                        bw config server "$BITWARDEN_SERVER_URL" >&2
+                        bw login --apikey >&2
+                        SESSION_TOKEN=$(bw unlock --raw --passwordenv BITWARDEN_MASTER_PASSWORD)
+                        bw get item "$ITEM_NAME" --session "$SESSION_TOKEN"
+                    ''',
                     returnStdout: true
                 ).trim()
             }
-
-            // def credential
-            // withEnv(["ITEM_NAME=${config.itemName}"]) {
-            //     def credentialJson = sh(
-            //         // Redirects output from most commands to avoid polluting the captured secret JSON
-            //         script: '''
-            //             set +x # Don't echo commands in logs
-            //             bw config server "$BITWARDEN_SERVER_URL" >&2
-            //             bw login --apikey >&2
-            //             SESSION_TOKEN=$(bw unlock --raw --passwordenv BITWARDEN_MASTER_PASSWORD)
-            //             bw get item "$ITEM_NAME" --session "$SESSION_TOKEN"
-            //         ''',
-            //         returnStdout: true
-            //     ).trim()
-            //     credential = readJSON text: credentialJson
-            // }
         } finally {
             sh 'bw logout || true' // Always logout even after failure
         }
